17588e6b471411c1731abdbd400ed2ae3cd87856
adriantelles <adriantellesweb@gmail.com>
2018-04-27 14:58:38 +0300

    Basic layouts

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>File Sharing</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+</head>
+<body>
+    @include("layouts.nav")
+
+    <main role="main" class="container">
+        @yield("content")
+    </main>
+</body>
+</html>
