751f4cd1cd6166b70b4585c33c1df347f2254264
adriantelles <adriantellesweb@gmail.com>
2018-06-18 01:02:58 +0300

    Created registration and login modals

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,100 @@
 
     <main role="main" class="container">
         @yield("content")
+
+        <div class="modal" id="registerModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form method="post" class="register-form" action="/register">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Registration</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                                {{ csrf_field() }}
+
+                                <div class="form-group">
+                                    <label for="username">Username:</label>
+                                    <input type="text" class="form-control @if ($errors->has("username")){{ "is-invalid" }}@endif"
+                                           id="username" name="username" value="{{ old("username") }}">
+                                    @if ($errors->has("username"))
+                                        <div class="invalid-feedback">{{ $errors->first("username") }}</div>
+                                    @endif
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">Email:</label>
+                                    <input type="email" class="form-control @if ($errors->has("email")){{ "is-invalid" }}@endif"
+                                           id="email" name="email" value="{{ old("email") }}">
+                                    @if ($errors->has("email"))
+                                        <div class="invalid-feedback">{{ $errors->first("email") }}</div>
+                                    @endif
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password">Password:</label>
+                                    <input type="password" class="form-control @if ($errors->has("email")){{ "is-invalid" }}@endif"
+                                           id="password" name="password">
+                                    @if ($errors->has("password"))
+                                        <div class="invalid-feedback">{{ $errors->first("password") }}</div>
+                                    @endif
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="password_confirmation">Confirm your password:</label>
+                                    <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
+                                </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+
+        <div class="modal" id="loginModal" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <form method="post" class="login-form" action="/login">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Logging in</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+
+                            {{ csrf_field() }}
+
+                            <div class="form-group">
+                                <label for="email">Email:</label>
+                                <input type="email" name="email" class="form-control" id="email">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" name="password" class="form-control" id="password">
+                            </div>
+
+                        </div>
+                        <div class="modal-footer">
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Log in</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
     </main>
 
     <script src="{{ asset("js/app.js") }}"></script>
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -12,11 +12,11 @@
 
             @if (!Auth::check())
                 <li class="nav-item">
-                    <a class="nav-link" href="/login">Sign In</a>
+                    <a class="nav-link" href="#loginModal" data-toggle="modal" data-target="#loginModal">Sign In</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link" href="/register">Sign Up</a>
+                    <a class="nav-link" href="#registerModal" data-toggle="modal" data-target="#registerModal">Sign Up</a>
                 </li>
             @endif
 
