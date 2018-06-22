2c62362ecf61d2e6609fc0b006b163bea509368b
adriantelles <adriantellesweb@gmail.com>
2018-06-22 19:48:25 +0300

    Added uploaded files metadata handling

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FileUploadRequest;
+use App\Services\FileService;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 
@@ -10,17 +11,23 @@ use Illuminate\Http\Request;
 class FilesController extends Controller
 {
     protected $getId3;
+    protected $fileService;
 
-    public function __construct()
+    public function __construct(FileService $fileService)
     {
         $this->getId3 = new \getID3();
+        $this->fileService = $fileService;
     }
 
     public function store(FileUploadRequest $request)
     {
+        $this->fileService->handleUploadedFile($request->file("file"));
+
+        /*
         $ext = $request->file("file")->getClientOriginalExtension();
         $request->file("file")->storeAs("files","1" . "." . $ext);
 
         dd($this->getId3->analyze(storage_path("app/files/1.{$ext}")));
+        */
     }
 }
--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -4,27 +4,72 @@ namespace App\Services;
 
 class FileService
 {
+    protected $getId3;
+
+    public function __construct()
+    {
+        $this->getId3 = new \getID3();
+    }
+
     public function handleUploadedFile($file)
     {
-        $originalName = $file->getClientOriginalName();
-        $originalExtenstion = $file->getClientOriginalExtension();
+        // Data to be inserted in DB
+        $metaDataForDB = [];
 
-        $requiredMetaData = [
-          "all" => "filesize",
-          "audio" => [
-              "general" => "playtime_string",
-              "audio" => "bitrate|sample_rate|channels|codec"
-          ],
-          "video" => [
-              "general" => "playtime_string",
-              "audio" => "sample_rate|channels|codec",
-              "video" => "resolution_x|resolution_y"
-          ],
-          "image" => [
-              "general" => "fileformat",
-              "video" => "bits_per_sample"
-          ]
-        ];
+        $originalName = $file->getClientOriginalName();
+        $originalExtension = $file->getClientOriginalExtension();
+
+        // Storing file
+        $pathToFile = $file->storeAs("files", $this->makeFileName($file));
+
+        // Analyzing the file
+        $fileInfo = $this->getId3->analyze(storage_path("app/".$pathToFile));
+
+        // Getting the MIME-type of the file
+        $mimeType = $fileInfo["mime_type"];
+
+        $metaDataForDB["filesize"] = $fileInfo["filesize"];
+        $metaDataForDB["extension"] = $originalExtension;
+
+        // Exploding the MIME-type by "/" and getting the first part of the MIME-type
+        $fileType = explode("/", $mimeType)[0];
+        switch ($fileType) {
+            case "audio":
+                $audioParameters = [
+                  "bitrate" => $fileInfo["audio"]["bitrate"],
+                  "sample_rate" => $fileInfo["audio"]["sample_rate"],
+                  "channels_count" => $fileInfo["audio"]["channels"],
+                  "codec" => $fileInfo["audio"]["codec"]
+                ];
+
+                $metaDataForDB["playtime_string"] = $fileInfo["playtime_string"];
+                $metaDataForDB["audio"] = $audioParameters;
+                break;
+            case "video":
+                $audioParameters = [
+                  "sample_rate" => $fileInfo["audio"]["sample_rate"],
+                  "channels_count" => $fileInfo["audio"]["channels"],
+                  "codec" => $fileInfo["audio"]["codec"]
+                ];
+                $videoParameters = [
+                  "resolution_x" => $fileInfo["video"]["resolution_x"],
+                  "resolution_y" => $fileInfo["video"]["resolution_y"]
+                ];
+
+                $metaDataForDB["playtime_string"] = $fileInfo["playtime_string"];
+                $metaDataForDB["audio"] = $audioParameters;
+                $metaDataForDB["video"] = $videoParameters;
+                break;
+            case "image":
+                $videoParameters = [
+                  "resolution_x" => $fileInfo["video"]["resolution_x"],
+                  "resolution_y" => $fileInfo["video"]["resolution_y"]
+                ];
+
+                $metaDataForDB["fileformat"] = $fileInfo["fileformat"];
+                $metaDataForDB["video"] = $videoParameters;
+                break;
+        }
     }
 
     public function makeFileName($file)
