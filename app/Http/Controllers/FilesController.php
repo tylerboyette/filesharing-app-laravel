5871c9a307b678f01158c2cca5019c071dec88e0
adriantelles <adriantellesweb@gmail.com>
2018-07-09 17:05:14 +0300

    Correctly displaying Content-Type during file download

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -37,9 +37,12 @@ class FilesController extends Controller
     {
         $file = File::where("id", $id)->firstOrFail();
         $size = $file->meta_data["filesize"];
+        $contentType = array_key_exists("mime_type",$file->meta_data) ?
+                        $file->meta_data["mime_type"] :
+                        "application/octet-stream";
 
         header('Content-Description: File Transfer');
-        header('Content-Type: application/octet-stream');
+        header("Content-Type: $contentType");
         header("Content-Disposition: attachment");
         header("Content-Length: $size");
 
