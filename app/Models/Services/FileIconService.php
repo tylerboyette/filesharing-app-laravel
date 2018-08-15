14904aeeed5b990cee64c1d377eb0bde54bc5a3d
adriantelles <adriantellesweb@gmail.com>
2018-08-15 18:03:46 +0300

    Created FileIconService

---
+++ app/Models/Services/FileIconService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models\Services;
+
+class FileIconService
+{
+    protected $supportedFileExtensions;
+
+    public function __construct()
+    {
+        $this->supportedFileExtensions = json_decode(
+            file_get_contents(base_path("node_modules/file-icon-vectors/dist/icons/vivid/catalog.json"))
+        );
+    }
+
+    public function hasRelatedIcon(string $fileExtension)
+    {
+        return in_array($fileExtension,$this->supportedFileExtensions) ? true : false;
+    }
+}
