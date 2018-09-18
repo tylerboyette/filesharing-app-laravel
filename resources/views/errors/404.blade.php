1af4325da942000c5932e19e393d7d7f0ec28c88
adriantelles <adriantellesweb@gmail.com>
2018-09-18 23:00:53 +0300

    Created custom 404 error page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends ("layouts.master")
+
+@section ("content")
+    <div class="vertical-center justify-content-center">
+        <div class="d-flex flex-column not-found-error">
+            <span>404</span>
+            <span>The page was not found</span>
+        </div>
+    </div>
+@endsection
